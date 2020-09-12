c5e1954ac2b91e3ab29068da8defc8b17a4697f2
Raveen K Sharma <raveen08@gmail.com>
2020-09-12 23:08:06 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,6 +53,9 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
   
-  
+  def self.find_by(x)
+    sql = "SELECT * FROM #{self.table_name} WHERE self.column_names = ?"
+    DB[:conn].execute(sql, x)
+  end
   
 end
